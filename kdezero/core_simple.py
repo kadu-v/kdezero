9c6a6cd695bbad12e2f2ebf80779437ceab6a181
kadu-v <kazuonsunday@yahoo.co.jp>
2021-02-15 22:22:40 +0900

    step1

---
+++ kdezero/core_simple.py
@@ -0,0 +1,18 @@
+# ==================================================
+# Variable
+# ==================================================
+# %%
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+# %%
+data = np.array(1.0)
+x = Variable(data)
+print(x.data)
+
+# %%
