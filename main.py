de474ef1648cab839b4ee87336a20a3afd4ca095
kadu-v <kazuonsunday@yahoo.co.jp>
2021-03-15 23:39:55 +0900

    step15

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ import numpy as np
 
 
 x = Variable(np.array(3.0))
-y = add(x, x)
+y = add(square(x), square(x))
 
 print(y.data)
 
-y.backward()
-print(x.grad)
-
-x.cleargrad()
-y = add(x, add(x, x))
 y.backward()
 print(x.grad)
 
