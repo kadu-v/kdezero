5a7461b3bf5551e842ffbd0b834b607f25e44b1f
kadu-v <kazuonsunday@yahoo.co.jp>
2021-03-20 23:56:39 +0900

    step23 Fix: packaging core_simple.py

---
+++ kdezero/__init__.py
@@ -0,0 +1,9 @@
+from kdezero.core_simple import Variable
+from kdezero.core_simple import Function
+from kdezero.core_simple import using_config
+from kdezero.core_simple import no_grad
+from kdezero.core_simple import as_array
+from kdezero.core_simple import as_variable
+from kdezero.core_simple import setup_varibale
+
+setup_varibale()
--- kdezero/core_simple.py
+++ kdezero/core_simple.py
@@ -82,14 +82,14 @@ class Variable:
 
         def add_func(f):
             if f not in seen_set:
-                funcs.append(f)
+                funappend(f)
                 seen_set.add(f)
-                funcs.sort(key=lambda x: x.generation)
+                funsort(key=lambda x: x.generation)
 
         add_func(self.creator)
 
         while funcs:
-            f = funcs.pop()
+            f = funpop()
             gys = [output().grad for output in f.outputs]
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
@@ -149,3 +149,137 @@ class Function:
 
     def backward(self, gy):
         raise NotImplementedError()
+
+
+# ==================================================================================================
+# Basic Functions: Neg, Add, Sub, Mul, Div, Pow, Exp
+# ==================================================================================================
+
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
+
+
+def neg(x):
+    return Neg()(x)
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        x1 = x1
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
+
+def add(x0, x1):
+    x1 = as_array(x1)
+    return Add()(x0, x1)
+
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+
+    def backward(self, gy):
+        return gy, -gy
+
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        x1 = x1
+        y = x0 * x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy * x1, gy * x0
+
+
+def mul(x0, x1):
+    x1 = as_array(x1)
+    return Mul()(x0, x1)
+
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div(x1, x0)
+
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c * x ** (c - 1) * gy
+        return gx
+
+
+def pow(x, c):
+    return Pow(c)(x)
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = np.exp(x) * gy
+        return gx
+
+
+def exp(x):
+    return Exp()(x)
+
+
+def setup_varibale():
+    Variable.__add__ = add
+    Variable.__radd__ = add
+    Variable.__mul__ = mul
+    Variable.__rmul__ = mul
+    Variable.__neg__ = neg
+    Variable.__sub__ = sub
+    Variable.__rsub__ = rsub
+    Variable.__truediv__ = div
+    Variable.__rtruediv__ = rdiv
+    Variable.__pow__ = pow
