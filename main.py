9f476e126b22135cda987f4010cbeafda5447fc5
kadu-v <kazuonsunday@yahoo.co.jp>
2021-06-17 21:25:20 +0900

    Step 28

--- main.py
+++ main.py
@@ -6,9 +6,24 @@ from kdezero.utils import _dot_func, _dot_var, plot_dot_graph
 import numpy as np
 
 
-x = Variable(np.array(np.pi / 4))
-y = F.sin(x)
-y.backward()
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
+    return y
 
-print(y)
-print(x.grad)
+
+x0 = Variable(np.array(0.0))
+x1 = Variable(np.array(2.0))
+lr = 0.001
+
+iters = 1000
+
+for i in range(iters):
+    print(x0, x1)
+
+    y = rosenbrock(x0, x1)
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
