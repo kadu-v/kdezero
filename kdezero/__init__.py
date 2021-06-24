164aa061b684a2d0663866924356a8ef5dd4e5f7
kadu-v <kazuonsunday@yahoo.co.jp>
2021-06-24 20:31:50 +0900

    Step 32

--- kdezero/__init__.py
+++ kdezero/__init__.py
@@ -1,9 +1,24 @@
-from kdezero.core_simple import Variable
-from kdezero.core_simple import Function
-from kdezero.core_simple import using_config
-from kdezero.core_simple import no_grad
-from kdezero.core_simple import as_array
-from kdezero.core_simple import as_variable
-from kdezero.core_simple import setup_varibale
+
+
+is_simple_core = False
+
+
+if is_simple_core:
+    from kdezero.core_simple import Variable
+    from kdezero.core_simple import Function
+    from kdezero.core_simple import using_config
+    from kdezero.core_simple import no_grad
+    from kdezero.core_simple import as_array
+    from kdezero.core_simple import as_variable
+    from kdezero.core_simple import setup_varibale
+else:
+    from kdezero.core import Variable
+    from kdezero.core import Function
+    from kdezero.core import using_config
+    from kdezero.core import no_grad
+    from kdezero.core import as_array
+    from kdezero.core import as_variable
+    from kdezero.core import setup_varibale
+
 
 setup_varibale()
