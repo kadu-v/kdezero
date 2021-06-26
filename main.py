3fcaef31c01ee5ff89a3519db7785c89aebe5a88
kadu-v <kazuonsunday@yahoo.co.jp>
2021-06-26 20:56:32 +0900

    Step 35

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ from kdezero.utils import _dot_func, _dot_var, plot_dot_graph
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = Variable(np.linspace(-7, 7, 200))
-y = F.sin(x)
+x = Variable(np.array(1.0))
+y = F.tanh(x)
+x.name = 'x'
+y.name = 'y'
 y.backward(create_graph=True)
-logs = [y.data.flatten()]
 
-for i in range(3):
-    logs.append(x.grad.data.flatten())
+iters = 3
+for i in range(iters):
     gx = x.grad
     x.cleargrad()
     gx.backward(create_graph=True)
 
-labels = ["y=sin(x)", "y'", "y''", "y'''"]
-for i, v in enumerate(logs):
-    plt.plot(x.data, logs[i], label=labels[i])
 
-plt.legend(loc="lower right")
-plt.show()
+gx = x.grad
+gx.name = 'gx' + str(iters + 1)
+plot_dot_graph(gx, verbose=False, to_file='tanh.png')
