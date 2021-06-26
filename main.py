cbda8ea757b46c9b91c51271a034ee310e9deb9f
kadu-v <kazuonsunday@yahoo.co.jp>
2021-06-26 21:14:17 +0900

    Step 36

--- main.py
+++ main.py
@@ -6,19 +6,12 @@ from kdezero.utils import _dot_func, _dot_var, plot_dot_graph
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = Variable(np.array(1.0))
-y = F.tanh(x)
-x.name = 'x'
-y.name = 'y'
+x = Variable(np.array(2.0))
+y = x ** 2
 y.backward(create_graph=True)
-
-iters = 3
-for i in range(iters):
-    gx = x.grad
-    x.cleargrad()
-    gx.backward(create_graph=True)
-
-
 gx = x.grad
-gx.name = 'gx' + str(iters + 1)
-plot_dot_graph(gx, verbose=False, to_file='tanh.png')
+x.cleargrad()
+
+z = gx ** 3 + y
+z.backward()
+print(x.grad)
