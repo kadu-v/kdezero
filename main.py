b4ec7c7fc9529bdcbe89a6bebb222ef07450aee0
kadu-v <kazuonsunday@yahoo.co.jp>
2021-06-28 21:54:20 +0900

    Step 38, 39

--- main.py
+++ main.py
@@ -6,13 +6,8 @@ from kdezero.utils import _dot_func, _dot_var, plot_dot_graph
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
-y = F.reshape(x, (6,))
-y.backward(retain_grad=True)
-print(x.grad)
-
-# %%
-x = Variable(np.random.randn(2, 3))
-y = x.transpose()
-y = x.transpose()
+x = Variable(np.array([1, 2, 3, 4, 5, 6]))
+y = F.sum(x)
+y.backward()
 print(y)
+print(x.grad)
